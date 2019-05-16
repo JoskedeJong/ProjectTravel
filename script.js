be7ad47cf672f81a5dc05731f60ae025ba6fa910
Joske de Jong <joskedejong@gmail.com>
2019-05-16 12:27:23 +0200

    made several smaller cleanup effeorts, most notably the button identifier. Storing in masterbranch before practicing with new branches.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var anyY = 0;
 var anyX = 0;
-
-var x = 1;
+var w = 0;
+var q = 0;
+var r = 0;
 var y = 0;
+var x = 0;
+var s = 0;
+var t = 0;
+var n = 0;
 var a = 0;
+var q = 0;
+var id = 0;
+var slice = 0;
 var travelItems = [[0,"Destination", "Rating", "Cost", "word", "_"],[1,"Moscow","4\/5","120\$","one","a"],[2,"Berlin","2\/5","80\$","two","aa"],[3,"New York","5\/5","230\$","three","aaa"],[4,"Sydney","3\/5","150\$", "four", "aaaa",]];
 var allSelected = [];
 var allIdentifiers = [];
@@ -15,50 +23,55 @@ var newArray = [];
 var subArray = [];
 var masterArray = [];
 var strLength = [];
-var t = 0;
-var q = 0;
+var selectForMeasure = [];
 
-var anyArray = travelItems;                                             // convert an array to var anyAray. This should accept all arrays
-
+var anyArray = travelItems;                                             // convert an array (in this case travelItems) to var anyAray. This should accept all arrays
 var anyY = anyArray.length;
 var anyX = anyArray[0].length;
 
-var tbl = document.createElement("TABLE");                              // breates basis for rest of table to be appended to
+var tbl = document.createElement("TABLE");                              // creates basis for rest of table to be appended to
 document.body.appendChild(tbl);
 tbl.setAttribute("border", "2");
 
-function arraySwitch(){
-    var w = masterArray.length;
-    if (w  !== 0){
-        console.log("blip");
-        anyArray = masterArray.slice(0);
-        // console.log(anyArray);
-        function empty() {
-            masterArray.length = 0;
-            allSelected.length = 0;
-            allIdentifiers.length = 0;
-            allCombined.length = 0;                                               // this conversion currently only works for single-digit row-identifiers, but can be modified for multi-digit
-            frontString.length = 0;
-            backtString.length = 0;
-            unstrung.length = 0;
-            newArray.length = 0;
-            subArray.length = 0;
-            strLength.length = 0;
-        }
-        empty();
-    }
+function emptyAll() {
+    console.log("all emptied");
+    masterArray.length = 0;
+    allSelected.length = 0;
+    allIdentifiers.length = 0;
+    allCombined.length = 0;                             // this conversion currently only works for single-digit row-identifiers, but can be modified for multi-digit
+    frontString.length = 0;
+    backtString.length = 0;
+    unstrung.length = 0;
+    newArray.length = 0;
+    subArray.length = 0;
+    strLength.length = 0;
+    selectForMeasure.length = 0;
+    slice.length = 0;
+    combineString = 0;
+    w = 0;
+    q = 0;
+    r = 0;
+    y = 0;
+    x = 0;
+    s = 0;
+    t = 0;
+    n = 0;
+    a = 0;
+    id = 0;
+    clickedId = 0;
 }
 
+function arraySwitch(){                                 // function that loops around resultant masterArray to starting anyArray
+    console.log("arrays switched");
+    anyArray.length = 0;                                // empties old input-array
+    anyArray = masterArray.slice(0);                    // copies output-array to input-array
+    masterArray.length = 0;                             // empties output-array
+}
 
+function sortFunction(){                                // function that sorts the selected variables from anyArray and produces a re-ordered masterArray
+    console.log("data sorted");
 
-function sortFunction(id){ 
-
-    var q = (id);
-                                                        /* why use a for loop? This function is used to pass through all properties and all identifiers for all possible property
-                                                        values in the chosen category. The chosen category shoud be in allSelected */
-                                               
-
-    for (a=0; a<anyY; a++){                             //the following function is supposed to take the values for the selected collumn (q) and the values for the identifier
+    for (a=0; a<anyY; a++){                             // this function is supposed to take the values for the selected collumn (q) and the values for the identifier
         var allSelected = anyArray [a][q];              // collumn, sting-ify them, add them together in one string, and then push them into a new array (allCombined)  
         var allIdentifiers = anyArray [a][0];           // [a]=value that passes all sub-arrays. [q] is location of to-be-sorted value. [0] is location of identifier.
         frontString = allSelected.toString();           // conversion from number to string
@@ -70,66 +83,63 @@ function sortFunction(id){
     }
 
     for (n=0; n<anyY; n++){                             // makes new array that only contains the identifiers, in sorted order
-
         var selectForMeasure = allCombined[n];
         var strLength = selectForMeasure.length;
         newArray.push(allCombined[n][(strLength - 1)]);
     }    
 
-    console.log(allCombined);
-
-    indexPicker = newArray.findIndex(searchFunction);   //function made to keep header values on top. this picks the values, the next function moves them.
+    indexPicker = newArray.findIndex(searchFunction);   //function made to keep table-header values on top. this picks the values, the next function moves them.
     function searchFunction(value){
         return value == 0;
     }
-    function removeHeaderValues(){                      //function made to keep header values on top
+    function removeHeaderValues(){                      // function made to keep header values on top
         newArray.splice(indexPicker, 1);                // removes element picked out by indexPicker
         newArray.unshift(0);                            // adds element 0 at start of array
     }
     removeHeaderValues(newArray);
 
-    for (t=0; t<anyY; t++){                             // t is order-value (location) of items in newArray, r is value of these items
+    for (t=0; t<anyY; t++){                             // t is order-value (location) of items in newArray, r is id-value of these items
         var r = newArray[t];
-        for (p=0; p<anyX; p++){                         /* for all the values (=t) values of newArray I want properties from item r  '1 through anyX', 
-                                                        except for the last one. p will go through all location values for array ith id r */                                             
-            subArray.push(anyArray[r][p]); 
+        for (p=0; p<anyX; p++){                         // for all the values (=t) values of newArray I want properties from item r  '1 through anyX'. 
+                                                        // p will go through all location values for array ith id r */                                             
+            subArray.push(anyArray[r][p]);              // subArray is now a one-dimensional string with all the values that should be in master Array
         }
     }
 
-    for (s=0; s<anyY; s++){
+    for (s=0; s<anyY; s++){                             // slices subArray into small arrays stored in 'slice', which are then pushed to masterArray
         var slice = subArray.slice((s * anyX),((s * anyX) + anyX));
         masterArray.push(slice);
-        var slice = "aaa";
     }
-    
+}    
+
+function buildTable(){                                                       // function that creates the table
+    console.log("table built");
     for (y=0; y < anyY; y++){
         var createRow = document.createElement("TR");
-        tbl.appendChild(createRow);s
+        tbl.appendChild(createRow);
         for (x=0; x < anyX; x++){
             var createData = document.createElement("TD");
             createRow.appendChild(createData);
             var contentData = document.createTextNode(anyArray[y][x]);    
             createData.appendChild(contentData);
             if ((x==(anyX -1)) && (y!=0)){
-                var createButton = document.createElement("BUTTON");         //creates right buttons
+                var createButton = document.createElement("BUTTON");         // creates right buttons
                 createData.appendChild(createButton);
             }
             if ((y==0) && (x!= anyX - 1)) {
                 var createButton = document.createElement("BUTTON");         // creates top buttons
-                createButton.setAttribute("onclick", "arraySwitch() ; sortFunction(id);");
                 createButton.setAttribute("id", x);
+                createButton.setAttribute("onclick", "buttonIdentifier("+x+"); sortFunction(); buildTable(); arraySwitch(); emptyAll()");
                 createData.appendChild(createButton);
             }
         }
     } 
-    console.log(masterArray);   
-                                                                            // if I want anything to happen after the buttons have been created, it should probably be done here.
 }
-sortFunction(0);                                    //perform sortFunction for value 0. This is here for when the page loads.
-  
 
-                                                    /* what do I want to achieve now? I want to read out every [1] character for each element of the string. To do what?
-                                                    I want these so I I have a reference to set up my new table. How do I set up my table with a reference to a array of 
-                                                    randomly ordered numbers? 
-                                                    What I could do is use the identifier to taget the right sub-array and attach the old content back to the identifier.
-                                                    if that is done, I technically have a  new travelItems*/
+function buttonIdentifier(clickedId){
+    q = clickedId;
+    console.log(q + " was clicked");
+}
+
+buildTable();
+                                                                            // if I want anything to happen after the buttons have been created, it should probably be done here.
